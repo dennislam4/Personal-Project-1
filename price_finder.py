eb6f88374a6937adb2cf30d8c1da5529b8c12e40
Dennis <lamden@oregonstate.edu>
2022-09-05 13:13:57 -0600

    Added if statement and function to have the program refresh and run every hour.

--- price_finder.py
+++ price_finder.py
@@ -4,24 +4,35 @@
 from bs4 import BeautifulSoup
 import requests
 import re
+import time
 
-#Requests from url and filtering out all hats
+#Requests from url and filtering out all hats.
 html_text = requests.get("https://www.baitme.com/headwear").text
 soup = BeautifulSoup(html_text, "lxml")
 hats = soup.find_all("li", class_ = 'item last')
 
-#Sorts through HTML Tags and creates variables corresponding from search
-for hat in hats:
-    product_name = hat.find("h2", class_ = "product-name").text
-    product_name = re.sub("\(.*?\)","()", product_name).replace("()","")
-    price = hat.find("span", class_ = "price").text
-    product_info = hat.div.h2.a["href"]
-    product_color = hat.find("h2", class_ = "product-name").text
-    product_color = re.search('\(([^)]+)', product_color).group(1)
+def price_finder():
+    """
+    Webscraper that scrapes baitme.com and lists out product name, color and prices of hats. Loops every hour.
+    """
+    for hat in hats:
+     # Sorts through HTML Tags and creates variables corresponding from search.
+        product_name = hat.find("h2", class_ = "product-name").text
+        product_name = re.sub("\(.*?\)","()", product_name).replace("()","")
+        price = hat.find("span", class_ = "price").text
+        product_info = hat.div.h2.a["href"]
+        product_color = hat.find("h2", class_ = "product-name").text
+        product_color = re.search('\(([^)]+)', product_color).group(1)
 
-#Print statements for output
-    print(f'Product Name: {product_name}')
-    print(f'Product Color: {product_color.title()} ')
-    print(f'Price: {price}')
-    print(f'Product Page: {product_info}')
-    print(" ")
+    #Print statements for output
+        print(f'Product Name: {product_name}')
+        print(f'Product Color: {product_color.title()} ')
+        print(f'Price: {price}')
+        print(f'Product Page: {product_info}')
+        print(" ")
+
+if __name__ == "__main__":
+    #Runs the function every hour.
+    while True:
+        price_finder()
+        time.sleep(3600)
